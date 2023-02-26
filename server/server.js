5301c5a400d4b874b2c15a5a9f38d256e3f53e69
ajaymude <mudeajay@gmail.com>
2023-02-26 08:32:17 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -1,16 +1,53 @@
-const express = require('express')
-const app = express()
-const port = process.env.port || 8000
-const dbconnection = require('./database/db')
-const router = require('./routes/userRouter')
-const cors = require('cors')
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-//db connection
-dbconnection()
 
-app.use('/', router)
 
-app.listen(port , ()=>{ console.log('server is runnig on port ')})
+
+dotenv.config();
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+    res.status(200).send({
+        message:
+            "This is ChatGPT AI APP server url, please visit https://chatgpt-ai-app-od21.onrender.com",
+    });
+});
+
+const configuration = new Configuration({
+    apiKey: process.env.Api_key,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post("/", async (req, res) => {
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: req.body.input,
+            temperature: 0,
+            max_tokens: 4000,
+            top_p: 1,
+            frequency_penalty: 0.5,
+            presence_penalty: 0,
+        });
+        console.log("PASSED: ", req.body.input);
+
+        res.status(200).send({
+            bot: response.data.choices[0].text,
+        });
+    } catch (error) {
+        console.log("FAILED:", req.body.input);
+        console.error(error);
+        res.status(500).send(error); 
+    }
+});
+
+
+app.listen(8000, () => console.log("Server is running on port 80000"  ));
